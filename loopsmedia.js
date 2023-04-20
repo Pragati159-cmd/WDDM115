942a0bada9ad4b1344af96b7f8ea9814ddf0fbf7
Pragati159-cmd <129121423+Pragati159-cmd@users.noreply.github.com>
2023-04-20 12:12:42 -0400

    test

--- loopsmedia.js
+++ loopsmedia.js
@@ -10,7 +10,7 @@ const media = [
 ];
 
 // loop through the array of media files and create HTML elements for each
-for (let i = 0; i < media.length; i++) {
+for (let i = 5; i < media.length; i++) {
   const file = media[i];
 
   if (file.endsWith(".mp4")) {
